987be6342dfa67dccee5ef7c4f5cbb68c5f1f9cc
wattanapong <wattanapong.su@outlook.com>
2014-03-26 16:05:58 +0700

    update bootstrap to register

--- register.php
+++ register.php
@@ -7,12 +7,17 @@ include 'config.php';
 <meta charset="UTF-8">
 <title>WebboardSE</title>
 <link rel="stylesheet" type="text/css" href="CSS/main.css">
+<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
 </head>
 <body>
 
 	<div class="main">
 		<div class="header"><h1><?php echo $webname; ?></h1></div>
-		<div class="body"> body </div>
+		<div class="body"> 
+		<form action="register.php" method="post">
+			<label></label>
+		</form>
+		</div>
 		<div class="footer">footer</div>
 		
 	</div>
