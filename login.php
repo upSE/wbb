3a12024582e1430f4cbd1cc3c4985e6d4abf7a90
Commonder <petagorusse@gmail.com>
2014-03-28 12:27:18 +0700

    login
    logintest

--- login.php
+++ login.php
@@ -108,29 +108,61 @@ include 'config.php';
 
 
 						<!--/stories-->
-						<?php  
-							$query = mysql_query("SELECT * FROM catagory") or die("cannot select catagory");
-							while($row = mysql_fetch_array( $query ) ){
-						?>
 						<div class="row">
-							<br>
-							<div class="col-md-2 col-sm-3 text-center">
-								<a class="story-title"
-									href="http://www.techvisually.com/p/2LmGb0yEsL"><img
-									alt="Repurpose Content to Reach a Wider Audience"
-									src="img/<?php echo $row['image']; ?>"
-									style="width: 100px; height: 100px" class="img-circle"> </a>
-							</div>
-							<div class="col-md-10 col-sm-9">
-								<h3>
-									
-									<?php echo $row['topic'];?>
-								</h3>
-								<span><?php echo $row['description'];?></span>
-							</div>
-						</div>
-						<hr>
-						<?php } ?>
+    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+		<form role="form">
+			<h1>ลงทะเบียน</h1>
+			<hr class="colorgraph">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<div class="form-group">
+                        <input type="text" name="first_name" id="first_name" class="form-control input-lg" placeholder="ชื่อ" tabindex="1">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<div class="form-group">
+						<input type="text" name="last_name" id="last_name" class="form-control input-lg" placeholder="นามสกุล" tabindex="2">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<input type="text" name="display_name" id="display_name" class="form-control input-lg" placeholder="ชื่อผู้ใช้" tabindex="3">
+			</div>
+			<div class="form-group">
+				<input type="email" name="email" id="email" class="form-control input-lg" placeholder="อีเมล์" tabindex="4">
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<div class="form-group">
+						<input type="password" name="password" id="password" class="form-control input-lg" placeholder="รหัสผ่าน" tabindex="5">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6">
+					<div class="form-group">
+						<input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="ยืนยันรหัสผ่าน" tabindex="6">
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-4 col-sm-3 col-md-3">
+					<span class="button-checkbox">
+						<!--<button type="button" class="btn" data-color="info" tabindex="7">I Agree</button>-->
+                        <input type="checkbox" name="t_and_c" id="t_and_c" class="hidden" value="1">
+					</span>
+				</div>
+				<!--<div class="col-xs-8 col-sm-9 col-md-9">
+					 By clicking <strong class="label label-primary">Register</strong>, you agree to the <a href="#" data-toggle="modal" data-target="#t_and_c_m">Terms and Conditions</a> set out by this site, including our Cookie Use.
+				</div>-->
+			</div>
+			
+			<hr class="colorgraph">
+			<div class="row">
+				<div class="col-xs-12 col-md-6"><input type="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
+				<div class="col-xs-12 col-md-6"><a href="#" class="btn btn-success btn-block btn-lg">Sign In</a></div>
+			</div>
+		</form>
+	</div>
+</div>
 
 						<!--/stories-->
 
